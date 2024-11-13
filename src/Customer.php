99803d6ab964ac184a7eaf577f36f11bd5505eca
Endi Prayitno <healthclop@gmail.com>
2024-11-13 20:49:14 +0700

    Contoh Library Sederhana Composer

---
+++ src/Customer.php
@@ -0,0 +1,11 @@
+<?php
+namespace Prayendi\Learn;
+class Customer{
+    public function __construct(private string $name)
+    {
+    }
+
+    public function sayHello(string $name): string{
+        return "Hello $name, my name is $this->name";
+    }
+}
